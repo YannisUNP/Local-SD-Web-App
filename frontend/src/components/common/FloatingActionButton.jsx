a4f52f6787f26509c9e269906585af4f9a6f9fdb
YannisUNP <141173494+YannisUNP@users.noreply.github.com>
2026-04-16 13:05:09 +0200

    Delete frontend directory

--- frontend/src/components/common/FloatingActionButton.jsx
+++
@@ -1,9 +0,0 @@
-const FloatingActionButton = () => {
-  return (
-    <button className="fixed bottom-8 right-8 w-14 h-14 bg-blue-600 text-white rounded-full">
-      +
-    </button>
-  );
-};
-
-export default FloatingActionButton;
--- frontend/src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
- const SUPABASE_URL="https://jwnrraxihfnzctktxwne.supabase.co"
- const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bnJyYXhpaGZuemN0a3R4d25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzc4NDYsImV4cCI6MjA5MDYxMzg0Nn0.C48DtJWcCdYAT-tY8btxJsdGGZBRbHQCKNw8hu2JfnQ"
-
- export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
